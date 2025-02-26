40e552aaf5141c2de44dff7ec842601549a8a088
arasmodirpolimi <arsalan.modirkhazeni@mail.polimi.it>
2025-02-26 12:09:23 +0100

    Adding a debug configuration

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Hi this is from typescript");
+let a = 2;
+let b = 3;
+let c = a + b;
+console.log(c);
+//# sourceMappingURL=script.js.map
---
+++ script.ts
@@ -0,0 +1,10 @@
+console.log("Hi this is from typescript");
+
+let a = 2;
+
+let b = 3;
+
+let c = a + b;
+
+console.log(c);
+
